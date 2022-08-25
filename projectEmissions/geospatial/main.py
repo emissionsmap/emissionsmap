400bc32304bb2eb982b0ef601270d2009a7276d0
Alex Rodriguez Villavicencio <arodriguezv@uni.pe>
2022-08-25 01:43:44 -0500

    feat: static files and scripts
    * add database connection
    
    * changes templates and css files

--- projectEmissions/geospatial/main.py
+++ projectEmissions/geospatial/main.py
@@ -1,6 +1,11 @@
 import folium
 
 def mapPlot():
-    m = folium.Map(location=[-9.189967, -75.015152], zoom_start=5)
+    m = folium.Map(location=[-9.189967, -75.015152],
+                    position='absolute',zoom_start=2,
+                    zoom_control = False, max_zoom=4,
+                    prefer_canvas=True)
+    	
+    folium.TileLayer('stamenwatercolor').add_to(m)
     folium.Marker(location=[-9.189967, -75.015152]).add_to(m)
     return m._repr_html_()
