abe325a6b298b9e8fd7e6a4d7ae25b590b47bab6
jhon <jhlirion@gmail.com>
2022-09-01 17:58:01 -0500

    modifi

--- GraficosW/funiconGraf.py
+++ GraficosW/funiconGraf.py
@@ -74,3 +74,57 @@ def lineal2Plot():
             margin=dict(l=10,r=10,b=10,t=10)
       )
       return fig
+
+
+
+def lineal2Plot2():
+    df02 = pd.read_csv(r'D:\emissionsmap\SubirData\archivosPrueba\Temperatura_anomalia.csv', sep=';', low_memory=False, encoding='utf-8')
+    df04 = pd.read_csv(r'D:\emissionsmap\SubirData\archivosPrueba\gases.csv')
+    
+    df04.rename(columns={'year':'Año'}, inplace=True)
+    df04 = df04.sort_values('Año')
+    df04['Dif_co2'] = df04['Media co2'].diff()
+    df04 = df04[['Año', 'Dif_co2']]
+    df02 = df02[['Año', 'Anomalia de la temperatura media del promedio de 1961 a 1990']]
+    df_n = pd.merge(df02, df04[['Año' ,'Dif_co2']], on=['Año'], how='left')
+    df_n = df_n[df_n['Año'] >= 1960]
+    
+    fig= go.Figure(data=[
+        go.Scatter(
+        x=df_n['Año'],
+        y=df_n['Dif_co2'],
+        mode='lines', 
+        name='Varición CO2',
+        line=dict(color='cyan')
+        ),
+        go.Scatter(
+        x=df_n['Año'],
+        y=df_n['Anomalia de la temperatura media del promedio de 1961 a 1990'],
+        mode='lines',
+        name='Variación Temperatura'
+        )
+      ])
+    fig.update_layout(
+        # title=name_title,
+        height=300,
+        plot_bgcolor='black',
+        paper_bgcolor= 'black',
+        font_color='#cee3e1',
+        legend=dict(
+                x=0.05,
+                y=1,
+                title_font_family="Times New Roman",
+                font=dict(
+                family="Courier",
+                size=12,
+                color="LightSteelBlue"
+                ),
+                bgcolor="Black",
+                bordercolor="LightSteelBlue",
+                borderwidth=1
+        ),
+        xaxis=dict(showgrid=False,showline=True,linecolor='rgb(255,255,255)'),
+        yaxis=dict(showgrid=False),
+        margin=dict(l=10,r=10,b=10,t=10)
+    )
+    return fig
